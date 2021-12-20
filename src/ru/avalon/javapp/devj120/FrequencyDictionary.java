54f9b106c44e87905b732a450efda3401f066e0d
dkory <d.koryakovsky@gmail.com>
2021-12-20 22:03:09 +0300

    some changes

--- src/ru/avalon/javapp/devj120/FrequencyDictionary.java
+++ src/ru/avalon/javapp/devj120/FrequencyDictionary.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class FrequencyDictionary {
     static Map<String, Integer > dictionaryList = new HashMap<>();
+    //если сделать так, то метод sortedMap можно убрать
+    // static Map<String, Integer > dictionaryList = new HashMap<>();
 
     // чтение файла
     public Map<String, Integer> read(String fileName) throws IOException {
@@ -23,7 +25,7 @@ public class FrequencyDictionary {
                         if (i!=s.length()-1) continue;
                     }
 
-                   if (!word.isEmpty()) {
+                    if (!word.isEmpty()) {
                         if (dictionaryList.containsKey(word.toString().toLowerCase(Locale.ROOT))) {
                             int savVal = dictionaryList.get(word.toString().toLowerCase(Locale.ROOT));
                             dictionaryList.put(word.toString().toLowerCase(Locale.ROOT), savVal+1);
@@ -33,7 +35,7 @@ public class FrequencyDictionary {
                             dictionaryList.put(word.toString().toLowerCase(), 1);
                             word.delete(0, word.length());
                         }
-                   }
+                    }
                 }
             }
         }
@@ -42,33 +44,32 @@ public class FrequencyDictionary {
 
     // запись результата в два файла отчета
     public static void saveReport() throws FileNotFoundException {
-            final Map<String , Integer> sorted = sortedMap(dictionaryList);
+        final Map<String , Integer> sorted = sortedMap(dictionaryList);
 
-            PrintWriter pw = new PrintWriter(Main.REPORT1);
-            sorted.forEach((k, v) -> {
-               pw.println(k + " relative=" + v + "  absolute=" + (double) v / sorted.size());
-            });
-
-
-
-            PrintWriter pw2 = new PrintWriter(Main.REPORT2);
-            // создадим компаратор, который будет сравнивать значения, а не ключи
-            Comparator<String> comparator = new Comparator<String>() {
-                public int compare(String k1, String k2) {
-                    dictionaryList.get(k1).compareTo(dictionaryList.get(k2));
-                    int compare = dictionaryList.get(k2).compareTo(dictionaryList.get(k1));
-                    if (compare == 0)
-                        return 1;
-                    return compare;
-                }
-            };
-            Map<String, Integer> sortedByValues = new TreeMap<>(comparator);
-            sortedByValues.putAll(sorted);
+        PrintWriter pw = new PrintWriter(Main.REPORT1);
+        sorted.forEach((k, v) -> {
+            pw.println(k + " relative=" + v + "  absolute=" + (double) v / sorted.size());
+        });
 
 
-            sortedByValues.forEach((k, v) -> {
-                pw2.println(k + " relative=" + v + "  absolute=" + (double) v / sorted.size());
-            });
+
+        PrintWriter pw2 = new PrintWriter(Main.REPORT2);
+        // создадим компаратор, который будет сравнивать значения, а не ключи
+        Comparator<String> comparator = new Comparator<String>() {
+            public int compare(String k1, String k2) {
+                int compare = dictionaryList.get(k2).compareTo(dictionaryList.get(k1));
+                if (compare == 0)
+                    return k1.compareTo(k2);
+                return compare;
+            }
+        };
+        Map<String, Integer> sortedByValues = new TreeMap<>(comparator);
+        sortedByValues.putAll(sorted);
+
+
+        sortedByValues.forEach((k, v) -> {
+            pw2.println(k + " relative=" + v + "  absolute=" + (double) v / sorted.size());
+        });
 /*          Первый вариант сортировки
             dictionaryList.entrySet().stream().sorted(Map.Entry.<String , Integer>comparingByValue().reversed())
                     .forEach(x -> pw.println(x.getKey() + "  relative=" + x.getValue() + " absolute="
